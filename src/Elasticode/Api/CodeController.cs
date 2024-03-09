9fc3b2dee120db26321a74fa5061f2dfbc8238a5
earloc <earloc@live.de>
2024-03-09 14:40:08 +0100

    feature: add API

---
+++ src/Elasticode/Api/CodeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Elasticode;
+
+[ApiController]
+[Route("api/code")]
+public class CodeController : Controller
+{
+    [HttpGet("classes")]
+    public IActionResult GetClasses() {
+        string[] classes = [
+            "foo",
+            "bar"
+        ];
+
+        return Ok(classes);
+    }
+}
