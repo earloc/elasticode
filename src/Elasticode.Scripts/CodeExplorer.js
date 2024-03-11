6460d0d4608de473381162832517f946d37bb7b2
earloc <earloc@live.de>
2024-03-11 17:56:23 +0100

    feature: use better sample

--- src/Elasticode.Scripts/CodeExplorer.js
+++ src/Elasticode.Scripts/CodeExplorer.js
@@ -1,113 +1,89 @@
-//import * as d3 from 'd3';
-
-const data = [{
-    name: "root",
-    children: [
-        {
-            name: "foo",
-            children: [
-                { name: "bar" },
-                { name: "foobar" },
-            ]
-        },
-        {
-            name: "oof",
-            children: [
-                { name: "rab" },
-                { name: "oofrab" },
-            ]
-        },
+const graph = ({
+    nodes: Array.from({length:13}, () => ({})),
+    links: [
+      { source: 0, target: 1 },
+      { source: 1, target: 2 },
+      { source: 2, target: 0 },
+      { source: 1, target: 3 },
+      { source: 3, target: 2 },
+      { source: 3, target: 4 },
+      { source: 4, target: 5 },
+      { source: 5, target: 6 },
+      { source: 5, target: 7 },
+      { source: 6, target: 7 },
+      { source: 6, target: 8 },
+      { source: 7, target: 8 },
+      { source: 9, target: 4 },
+      { source: 9, target: 11 },
+      { source: 9, target: 10 },
+      { source: 10, target: 11 },
+      { source: 11, target: 12 },
+      { source: 12, target: 10 }
     ]
-}]
+});
 
-
-const drag = simulation => {
-  
-  function dragstarted(event, d) {
-    if (!event.active) simulation.alphaTarget(0.3).restart();
-    d.fx = d.x;
-    d.fy = d.y;
-  }
-  
-  function dragged(event, d) {
-    d.fx = event.x;
-    d.fy = event.y;
-  }
-  
-  function dragended(event, d) {
-    if (!event.active) simulation.alphaTarget(0);
-    d.fx = null;
-    d.fy = null;
-  }
-  
-  return d3.drag()
-      .on("start", dragstarted)
-      .on("drag", dragged)
-      .on("end", dragended);
-}
-
-export function showGraph() {
-    // Specify the chart’s dimensions.
-    const width = 928;
-    const height = 600;
-
-    // Compute the graph and start the force simulation.
-    const root = d3.hierarchy(data);
-    const links = root.links();
-    const nodes = root.descendants();
-
-    const simulation = d3.forceSimulation(nodes)
-        .force("link", d3.forceLink(links).id(d => d.id).distance(0).strength(1))
-        .force("charge", d3.forceManyBody().strength(-50))
-        .force("x", d3.forceX())
-        .force("y", d3.forceY());
-
-    // Create the container SVG.
-    const svg = d3.create("svg")
-        .attr("width", width)
-        .attr("height", height)
-        .attr("viewBox", [-width / 2, -height / 2, width, height])
-        .attr("style", "max-width: 100%; height: auto;");
-
-    // Append links.
-    const link = svg.append("g")
-        .attr("stroke", "#999")
-        .attr("stroke-opacity", 0.6)
-    .selectAll("line")
-    .data(links)
-    .join("line");
-
-    // Append nodes.
-    const node = svg.append("g")
-        .attr("fill", "#fff")
-        .attr("stroke", "#000")
-        .attr("stroke-width", 1.5)
-    .selectAll("circle")
-    .data(nodes)
-    .join("circle")
-        .attr("fill", d => d.children ? null : "#000")
-        .attr("stroke", d => d.children ? null : "#fff")
-        .attr("r", 5)
-        .call(drag(simulation));
-
-    // node.append("title")
-    //     .text(d => d.data.name);
-
-    simulation.on("tick", () => {
+  function tick() {
     link
-        .attr("x1", d => d.source.x)
-        .attr("y1", d => d.source.y)
-        .attr("x2", d => d.target.x)
-        .attr("y2", d => d.target.y);
-
+    .attr("x1", d => d.source.x)
+    .attr("y1", d => d.source.y)
+    .attr("x2", d => d.target.x)
+    .attr("y2", d => d.target.y);
     node
-        .attr("cx", d => d.x)
-        .attr("cy", d => d.y);
-    });
-
-    //invalidation.then(() => simulation.stop());
-
-    const chart = svg.node();
-
-    container.append(chart);
+    .attr("cx", d => d.x)
+    .attr("cy", d => d.y);
 }
+
+function click(event, d) {
+    delete d.fx;
+    delete d.fy;
+    d3.select(this).classed("fixed", false);
+    simulation.alpha(1).restart();
+}
+
+function dragstart() {
+    d3.select(this).classed("fixed", true);
+}
+
+function dragged(event, d) {
+    d.fx = clamp(event.x, 0, width);
+    d.fy = clamp(event.y, 0, height);
+    simulation.alpha(1).restart();
+}
+
+
+const svg = d3.create("svg").attr("viewBox", [0, 0, width, height]),
+    link = svg
+    .selectAll(".link")
+    .data(graph.links)
+    .join("line")
+    .classed("link", true),
+    node = svg
+    .selectAll(".node")
+    .data(graph.nodes)
+    .join("circle")
+    .attr("r", 12)
+    .classed("node", true)
+    .classed("fixed", d => d.fx !== undefined);
+
+yield svg.node();
+
+const simulation = d3
+    .forceSimulation()
+    .nodes(graph.nodes)
+    .force("charge", d3.forceManyBody())
+    .force("center", d3.forceCenter(width / 2, height / 2))
+    .force("link", d3.forceLink(graph.links))
+    .on("tick", tick);
+
+const drag = d3
+    .drag()
+    .on("start", dragstart)
+    .on("drag", dragged);
+
+node.call(drag).on("click", click);
+
+container.append(svg);
+
+
+  
+
