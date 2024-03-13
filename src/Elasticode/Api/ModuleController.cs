71dd24b6559974bb879265113d6afc871f488c9d
earloc <earloc@live.de>
2024-03-13 18:54:35 +0100

    refactor: rename types

---
+++ src/Elasticode/Api/ModuleController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Elasticode;
+
+[ApiController]
+[Route("api/code/modules")]
+public class ModuleController : Controller
+{
+    [HttpGet("classes")]
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(ModuleModel[]))]
+    public IActionResult GetModules() {
+        ModuleModel[] classes = [
+            new("foo", []),
+            new("bar", []),
+            new("foobar", ["foo", "bar"]),
+            new("oof", ["rab"]),
+            new("rab", []),
+            new("oofrab", ["foobar", "oof"]),
+        ];
+
+        return Ok(classes);
+    }
+}
