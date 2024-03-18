e1dcda9bddca6f6dea342beb147df4c49ef94586
earloc <earloc@live.de>
2024-03-18 17:31:43 +0100

    wip: start implementing anchor aware joints

--- src/Elasticode/Api/ModuleModel.cs
+++ src/Elasticode/Api/ModuleModel.cs
@@ -2,6 +2,7 @@ namespace Elasticode;
 
 public class ModuleModel (string name, IEnumerable<string> uses) {
 
+    public bool IsVisible { get; set; } = true;
     public string Name { get; set; } = name;
 
     public IEnumerable<string> Uses { get; set; } = uses;
